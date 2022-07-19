911f51481085f277da1140717ea06b48fa577b04
Joaquin <joacosanchez20@gmail.com>
2022-07-19 19:50:24 -0300

    change urls of server

--- src/enums/enums.ts
+++ src/enums/enums.ts
@@ -1,7 +1,7 @@
 export enum URLS {
-    PORTFOLIO = 'http://localhost:8136/api/v1/portfolio',
-    HOLDINGS = 'http://localhost:8136/api/v1/portfolio/holdings',
-    UPDATEPORTFOLIO = 'http://localhost:8136/api/v1/portfolio/update'
+    PORTFOLIO = 'https://portfolio-tracker-server-js.herokuapp.com/api/v1/portfolio',
+    HOLDINGS = 'https://portfolio-tracker-server-js.herokuapp.com/api/v1/portfolio/holdings',
+    UPDATEPORTFOLIO = 'https://portfolio-tracker-server-js.herokuapp.com/api/v1/portfolio/update'
 }
 
 /* local
